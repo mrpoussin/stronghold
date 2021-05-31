fe21c7909784771baa8d6ef025555a2e8f127212
Guillaume Collet <guillaume@ionenergy.co>
2021-05-31 19:02:33 +0200

    adds console logs for first debug

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,9 @@
-Hooks.once('init', async function() {
+console.log("Hello World! This code runs immediately when the file is loaded.");
 
+Hooks.on("init", async function() {
+  console.log("This code runs once the Foundry VTT software begins it's initialization workflow.");
 });
 
-Hooks.once('ready', async function() {
-
+Hooks.on("ready", async function() {
+  console.log("This code runs once core initialization is ready and game data is available.");
 });
